9fa5cfb7ccb9de8c68fe52ef7835130c9475e8cd
Niall Robinson <niall.h.robinson@gmail.com>
2015-07-24 14:24:08 +0100

    new

--- js/controls/DeviceOrientationController.js
+++ js/controls/DeviceOrientationController.js
@@ -454,7 +454,7 @@ var DeviceOrientationController = function ( object, domElement ) {
 
 				}
 
-				document.getElementById("socket-id").innerHTML = turnspeed * (beta || 0) * delta; //camMoveFromRot( beta, object, delta, turnspeed);
+				document.getElementById("socket-id").innerHTML = object.rotation;//turnspeed * (beta || 0) * delta; //camMoveFromRot( beta, object, delta, turnspeed);
 
 				if ( this.freeze ) return;
 
